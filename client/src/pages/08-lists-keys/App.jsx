e47a2b755d6a5d530f8fe0a98a76f46236a29a9a
JekenMaharjan <maharjanjeken@gmail.com>
2026-02-07 00:34:21 +0545

    Day 8 : Simple list rendering and Object list with keys - Complete

--- client/src/pages/08-lists-keys/App.jsx
+++ client/src/pages/08-lists-keys/App.jsx
@@ -1,8 +1,28 @@
+import ListDemo from "./ListDemo";
 
 const ListsKeysPage = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <div>
-            <h1>Lists and Keys in React</h1>
+        <div className="flex flex-col items-center justify-between min-w-screen min-h-screen">
+            {/* HEADER SECTION */}
+            <header className="font-serif font-bold text-3xl mb-10 bg-blue-600 text-white w-full text-center p-5">
+                <h1>
+                    Day 8 - React Fundamentals : Lists & Keys in React
+                </h1>
+            </header>
+
+            {/* MAIN CONTENT */}
+            <main className="flex-1 flex w-full p-5 justify-center items-center">
+                <ListDemo />
+            </main>
+
+            {/* FOOTER SECTION */}
+            <footer className="flex w-full p-5 justify-center bg-gray-800">
+                <p className="text-center text-white font-serif">
+                    © {currentYear} React Practice
+                </p>
+            </footer>
         </div>
     );
 };
--- client/src/pages/08-lists-keys/ListDemo.jsx
+++ client/src/pages/08-lists-keys/ListDemo.jsx
@@ -1,7 +1,41 @@
 const ListDemo = () => {
+    // Simple list
+    const fruits = ["Apple", "Banana", "Mango"];
+
+    // List of Objects
+    const users = [
+        { id: 1, name: "Ram" },
+        { id: 2, name: "Shyam" },
+        { id: 3, name: "Hari" },
+    ];
+
     return (
-        <div>
-            <h1>Demo of List</h1>
+        <div className="flex flex-col gap-5 bg-gray-300 p-5 rounded-md shadow-lg">
+            <h3 className="font-semibold font-serif text-xl">List of Fruits</h3>
+            <ul className="flex flex-col gap-3">
+                {fruits.map((fruit, index) => (
+                    <li 
+                        key={index}
+                        className="bg-gray-200 py-2 px-4 rounded-md"
+                    >
+                        {fruit}
+                    </li>
+                ))}
+            </ul>
+
+            <h3 className="font-semibold font-serif text-xl">List of Users</h3>
+            <ul className="flex flex-col gap-3">
+                {users.map((user) => (
+                    <li 
+                        key={user.id}
+                        className="bg-gray-200 py-2 px-4 rounded-md"
+                    >
+                        {user.name}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
+
+export default ListDemo;
