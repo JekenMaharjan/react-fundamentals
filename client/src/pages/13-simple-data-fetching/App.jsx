f6766e0fee248cf132351a2ae785716390600af2
JekenMaharjan <maharjanjeken@gmail.com>
2026-02-07 20:00:58 +0545

    Day 13: Main page setup with header, main and footer sections

--- client/src/pages/13-simple-data-fetching/App.jsx
+++ client/src/pages/13-simple-data-fetching/App.jsx
@@ -1,8 +1,28 @@
+import FetchUsers from "./FetchUsers";
 
 const SimpleDataFetchingPage = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <div>
-            
+        <div className="flex flex-col items-center justify-between min-w-screen min-h-screen">
+            {/* HEADER SECTION */}
+            <header className="font-serif font-bold text-3xl mb-10 bg-blue-600 text-white w-full text-center p-5">
+                <h1>
+                    Day 13 — Simple Data Fetching
+                </h1>
+            </header>
+
+            {/* MAIN CONTENT */}
+            <main className="flex-1 flex w-full p-5 justify-center items-center">
+                <FetchUsers />
+            </main>
+
+            {/* FOOTER SECTION */}
+            <footer className="flex w-full p-5 justify-center bg-gray-800">
+                <p className="text-center text-white font-serif">
+                    © {currentYear} React Practice
+                </p>
+            </footer>
         </div>
     );
 };
